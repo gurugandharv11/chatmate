2091ea61c7071737036e9cb24ad3f2ea1a13fa0e
gurugandharv11 <167278123+gurugandharv11@users.noreply.github.com>
2024-12-25 23:19:41 +0530

    Update UserRegistrationServlet.java

--- Review3,4/Review 3/JSP with servlets/Servlets/UserRegistrationServlet.java
+++ Review3,4/Review 3/JSP with servlets/Servlets/UserRegistrationServlet.java
@@ -1,1 +1,31 @@
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/register")
+public class UserRegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Get form data
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String age = request.getParameter("age");
+
+        // Store data in session
+        HttpSession session = request.getSession();
+        session.setAttribute("name", name);
+        session.setAttribute("email", email);
+        session.setAttribute("age", age);
+
+        // Redirect to the profile page
+        response.sendRedirect("profile");
+    }
+}
