8ccb930ae30a5604fc2c5aa06aedf8b5624b397e
gurugandharv11 <167278123+gurugandharv11@users.noreply.github.com>
2024-12-25 23:20:21 +0530

    Create UserProfileServlet.java

---
+++ Review3,4/Review 3/JSP with servlets/Servlets/UserProfileServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/profile")
+public class UserProfileServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Forward to profile.jsp
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+    }
+}
